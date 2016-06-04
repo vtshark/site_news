5f2405a4942bd0ec72ccf8a4899c3260156c9a72
Your Name <you@example.com>
2016-06-04 18:17:32 +0300

    comm3

--- templates/head.php
+++ templates/head.php
@@ -8,8 +8,6 @@
     <link href="<?=ROOT?>static/css/bootstrap.css" rel="stylesheet">
     <script src="<?=ROOT?>static/js/bootstrap.js"></script>
 
-
-
     <script type="text/javascript" src="<?=ROOT?>static/js/script.js"></script>
     <link rel="stylesheet" type="text/css" href="<?=ROOT?>static/css/style.css">
 </head>
